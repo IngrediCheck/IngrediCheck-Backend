9672333d511971bf4196d868d81faec8e72d4b59
justanotheratom <sanketpatel.1805@gmail.com>
2024-01-31 15:53:27 -0800

    driver.ts: fetch barcode details

--- local/driver.ts
+++ local/driver.ts
@@ -2,3 +2,13 @@
 import { supabase, signIn } from './kitchensink.ts'
 
 await signIn()
+
+const barcode = '028400310413'
+
+const result = await supabase.functions.invoke(`safeeats/inventory/${barcode}`, { method: 'GET' })
+
+if (result.error) {
+    console.log(`Error fetching inventory: ${result.error.message}`)
+}
+
+console.log(result.data)
