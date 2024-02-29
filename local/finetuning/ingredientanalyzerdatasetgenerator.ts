8cf9f734aa3dfa16f713190f053b98a47057a31f
justanotheratom <sanketpatel.1805@gmail.com>
2024-02-28 17:51:13 -0800

    IngredientAnalyzerAgent - Finetune GPT 3.5 Turbo 0125 with a few more examples

--- local/finetuning/ingredientanalyzerdatasetgenerator.ts
+++ local/finetuning/ingredientanalyzerdatasetgenerator.ts
@@ -312,6 +312,218 @@ ENRICHED UNBLEACHED FLOUR (WHEAT FLOUR, NIACIN, REDUCED IRON, THIAMIN MONONITRAT
             }
         }
     ],
+    [
+        {
+            role: 'user',
+            content:
+`My dietary preferences and restrictions:
+No animal products except eggs and dairy are ok
+Flag aspartame and high fructose corn syrup
+No seed oils
+No added sugar 
+No eggs no gelatin
+No high fructose corn syrup
+I don’t like vanilla flavoring
+A friend recommended to avoid palm oil
+I can’t stand the taste of cardamom
+I don’t feel good after eating garlic
+---------------------
+Analyze this product:
+Name: Keto Nut Granola
+Brand: null
+Ingredients:
+SUNFLOWER SEEDS, PUMPKIN SEEDS, COCONUT, ALMONDS, PECANS, ERYTHRITOL, BUTTER (CREAM, NATURAL FLAVOR), BLUEBERRIES, CINNAMON, SALT, MONK FRUIT EXTRACT
+`
+        },
+        {
+            role: 'assistant',
+            function_call: {
+                name: "record_not_safe_to_eat",
+                arguments: JSON.stringify({
+                    ingredients: []
+                })
+            }
+        }
+    ],
+    [
+        {
+            role: 'user',
+            content:
+`My dietary preferences and restrictions:
+No animal products except eggs and dairy are ok
+No seed oils
+No added sugar 
+No eggs no gelatin
+I don’t like vanilla flavoring
+---------------------
+Analyze this product:
+Name: Pepermint hot chocolate
+Brand: Honeypot Treats
+Ingredients:
+Bittersweet chocolate: (sugar, chocolate liquor, cocoa butter, natural vanilla flavor, soy lecithin (emulsifier))
+cane sugar
+coconut oil
+non fat milk powder
+cocoa
+maltodextrin: (corn)
+natural mint flavor
+tricalcium phosphate
+guar gum
+sodium caseinate: (a milk derivative)
+mono- and diglycerides
+dipotassium phosphate
+silicon dioxide
+salt
+`
+        },
+        {
+            role: 'assistant',
+            function_call: {
+                name: "record_not_safe_to_eat",
+                arguments: JSON.stringify({
+                    ingredients: [
+                        { ingredientName: "sugar", safetyRecommendation: "DefinitelyUnsafe", preference: "No added sugar", reasoning: "self-explanatory" },
+                        { ingredientName: "cane sugar", safetyRecommendation: "DefinitelyUnsafe", preference: "No added sugar", reasoning: "self-explanatory" },
+                        { ingredientName: "natural vanilla flavor", safetyRecommendation: "DefinitelyUnsafe", preference: "I don't like vanilla flavoring", reasoning: "self-explanatory" },
+                        { ingredientName: "mono- and diglycerides", safetyRecommendation: "MaybeUnsafe", preference: "No animal products except eggs and dairy are ok", reasoning: "Source of the mono- and diglycerides could be animal-based." },
+                        { ingredientName: "mono- and diglycerides", safetyRecommendation: "MaybeUnsafe", preference: "No seed oils", reasoning: "Mono- and diglycerides may have been derived from seed oils." }
+                    ]
+                })
+            }
+        }
+    ],
+    [
+        {
+            role: 'user',
+            content:
+`My dietary preferences and restrictions:
+No animal products except eggs and dairy are ok
+No seed oils
+No added sugar 
+---------------------
+Analyze this product:
+Name: White corn tortilla chips
+Brand: null
+Ingredients:
+stone ground white corn, vegetable oil (sunflower, safflower, canola)
+`
+        },
+        {
+            role: 'assistant',
+            function_call: {
+                name: "record_not_safe_to_eat",
+                arguments: JSON.stringify({
+                    ingredients: [
+                        { ingredientName: "sunflower", safetyRecommendation: "DefinitelyUnsafe", preference: "No seed oils", reasoning: "Sunflower Oil is a seed oil." },
+                        { ingredientName: "safflower", safetyRecommendation: "DefinitelyUnsafe", preference: "No seed oils", reasoning: "Safflower Oil is a seed oil." },
+                        { ingredientName: "canola", safetyRecommendation: "DefinitelyUnsafe", preference: "No seed oils", reasoning: "Canola Oil is a seed oil." },
+                    ]
+                })
+            }
+        }
+    ],
+    [
+        {
+            role: 'user',
+            content:
+`My dietary preferences and restrictions:
+No seed oils
+No added sugar 
+No eggs no gelatin
+I don’t feel good after eating garlic
+---------------------
+Analyze this product:
+Name: Roasted Hatch Chile & Jalapeno Cheese Dip
+Brand: Trader Joe's
+Ingredients:
+Sour Cream (Cultured Pasteurized Cream), Sharp Cheddar Cheese (Pasteurized Milk, Cheese Cultures, Salt, Microbial Enzymes, Annatto Extract (Color)), Cream Cheese (Pasteurized Cultured Milk and Cream, Salt, Stabilizers (Carob Bean Gum and Guar Gum)), Roasted Hatch Green Chile Peppers, Roasted Green Jalapeno Peppers, Jalapeno Peppers, Cornstarch, Sea Salt, Roasted Garlic Puree, Yeast Extract, Onion Powder, Smoked Paprika (Paprika, Rosemary Extract [To Preserve])
+`
+        },
+        {
+            role: 'assistant',
+            function_call: {
+                name: "record_not_safe_to_eat",
+                arguments: JSON.stringify({
+                    ingredients: [
+                        { ingredientName: "Roasted Garlic Puree", safetyRecommendation: "DefinitelyUnsafe", preference: "I don’t feel good after eating garlic", reasoning: "self-explanatory" }
+                    ]
+                })
+            }
+        }
+    ],
+    [
+        {
+            role: 'user',
+            content:
+`My dietary preferences and restrictions:
+No seed oils
+No added sugar 
+No eggs no gelatin
+---------------------
+Analyze this product:
+Name: Simply Nutty Dark Chocolate, Peanut & Almond Bar
+Brand: Trader Joe's
+Ingredients:
+Peanuts
+Almonds
+Inulin (Dietary Fiber)
+Dark Chocolate: (Sugar, Chocolate Liquor Processed with Alkali, Cocoa Butter, Soy Lecithin (Emulsifier), Vanilla Extract)
+Honey
+Pea Protein Crisp Rice: (Pea Protein Concentrate, Brown Rice Flour (Rice Flour, Rice Bran))
+Pea Protein Isolate
+Sea Salt
+Expeller Pressed Canola Oil
+Peanut Oil
+Vegetable Glycerin
+Soy Lecithin
+`
+        },
+        {
+            role: 'assistant',
+            function_call: {
+                name: "record_not_safe_to_eat",
+                arguments: JSON.stringify({
+                    ingredients: [
+                        { ingredientName: "Expeller Pressed Canola Oil", safetyRecommendation: "DefinitelyUnsafe", preference: "No seed oils", reasoning: "Canola Oil is a seed oil." },
+                        { ingredientName: "Peanut Oil", safetyRecommendation: "DefinitelyUnsafe", preference: "No seed oils", reasoning: "Peanut Oil is a seed oil." },
+                        { ingredientName: "Honey", safetyRecommendation: "DefinitelyUnsafe", preference: "No added sugar", reasoning: "Honey is a form of added sugar." },
+                    ]
+                })
+            }
+        }
+    ],
+    [
+        {
+            role: 'user',
+            content:
+`My dietary preferences and restrictions:
+No seed oils
+No added sugar 
+No eggs no gelatin
+I don’t like vanilla flavoring
+A friend recommended to avoid palm oil
+---------------------
+Analyze this product:
+Name: Simply Nutty Bars
+Brand: null
+Ingredients:
+Peanuts, Almonds, Walnuts, Dark chocolate (sugar, chocolate liquor, cocoa butter, soy lecithin, vanilla extract), Honey, Pea protein crisp rice (pea protein, brown rice flour, rice flour, rice bran), Pea protein isolate, Sea salt, Vanilla extract, canola oil, Vegetable glycerin
+`
+        },
+        {
+            role: 'assistant',
+            function_call: {
+                name: "record_not_safe_to_eat",
+                arguments: JSON.stringify({
+                    ingredients: [
+                        { ingredientName: "canola oil", safetyRecommendation: "DefinitelyUnsafe", preference: "No seed oils", reasoning: "Canola Oil is a seed oil." },
+                        { ingredientName: "Honey", safetyRecommendation: "DefinitelyUnsafe", preference: "No added sugar", reasoning: "Honey is a form of added sugar." },
+                        { ingredientName: "vanilla extract", safetyRecommendation: "DefinitelyUnsafe", preference: "I don’t like vanilla flavoring", reasoning: "self-explanatory" },
+                    ]
+                })
+            }
+        }
+    ]
 ]
 
 const finetuningData = trainingData.map((messages: any) => {
